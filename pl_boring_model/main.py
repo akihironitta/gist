4a1a26fd71dea23ec6ba62b064d93ba612bd75a5
Akihiro Nitta <akihiro@pytorchlightning.ai>
2022-07-21 03:49:37 -0400

    Update pl_boring_model

--- pl_boring_model/main.py
+++ pl_boring_model/main.py
@@ -37,26 +37,44 @@ class BoringModel(LightningModule):
         loss = self(batch).sum()
         self.log("test_loss", loss)
 
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure, **kwargs):
+        scaler = getattr(self.trainer.strategy.precision_plugin, "scaler", None)
+
+        if scaler:
+            scale_before_step = scaler.get_scale()
+            optimizer.step(closure=optimizer_closure)
+            scale_after_step = scaler.get_scale()
+
+        self.should_skip_lr_scheduler_step = scale_before_step > scale_after_step
+
+    def lr_scheduler_step(self, scheduler, optimizer_idx, metric):
+        if self.should_skip_lr_scheduler_step:
+            return
+
+        scheduler.step()
+
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
-        return [optimizer], [lr_scheduler]
+        scheduler = {
+            "scheduler": torch.optim.lr_scheduler.StepLR(optimizer, step_size=1),
+            "interval": "step",
+            "frequency": 1,  # other small numbers may also cause this issue.
+        }
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
 
 def main():
     train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
     val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
 
     model = BoringModel()
     trainer = Trainer(
         max_epochs=1,
-        accelerator="auto",
-        devices="auto",
-        benchmark=False,  # True by default in 1.6.{0-3}.
+        accelerator="gpu",
+        devices=1,
+        precision=16,
     )
     trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
 
 
 if __name__ == "__main__":
