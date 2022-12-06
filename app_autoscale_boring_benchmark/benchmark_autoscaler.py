c8e145f4147a33e0a17b0d8640551550b5097725
Akihiro Nitta <nitta@akihironitta.com>
2022-12-06 21:07:55 +0900

    Benchmark 1 to 30 works

--- app_autoscale_boring_benchmark/benchmark_autoscaler.py
+++ app_autoscale_boring_benchmark/benchmark_autoscaler.py
@@ -22,12 +22,13 @@ class BatchResponse(BaseModel):
 class MyPythonServer(L.app.components.PythonServer):
     def __init__(self, *args, **kwargs):
         super().__init__(
+            # cloud_compute=L.CloudCompute("cpu-small"),
             port=L.app.utilities.network.find_free_network_port(),
             input_type=BatchRequestModel,
             output_type=BatchResponse,
             start_with_flow=False,
         )
-        self._time_requested_at = time.time()
+        self._time_requested_at = kwargs["time_requested_at"]
         self.drive = L.app.storage.Drive(f"lit://drive")
 
     def run(self, *args: Any, **kwargs: Any) -> Any:
@@ -51,44 +52,69 @@ class MyAutoScaler(L.app.components.AutoScaler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def create_work(self):
-        # TODO: Remove `start_with_flow=False` for faster initialization on the cloud
-        return self._work_cls(*self._work_args, **self._work_kwargs, start_with_flow=False)
+    def create_work(self, time_requested_at=time.time()):
+        return self._work_cls(*self._work_args, **self._work_kwargs, time_requested_at=time_requested_at)
 
     def scale(self, replicas: int, metrics: dict) -> int:
-        # scale out if the number of pending requests exceeds max batch size.
-        max_requests_per_work = self.max_batch_size
         pending_requests_per_running_or_pending_work = metrics["pending_requests"] / (
             replicas + metrics["pending_works"]
         )
-        min_requests_per_work = max_requests_per_work * 0.25
-        pending_requests_per_running_work = metrics["pending_requests"] / replicas
-        print(
-            f"replicas={replicas}, "
-            f"max_requests_per_work={max_requests_per_work}, "
-            f"pending_works={metrics['pending_works']}, "
-            f"pending_requests={metrics['pending_requests']}, "
-            f"pending_requests_per_running_or_pending_work={pending_requests_per_running_or_pending_work}, "
-            f"min_requests_per_work={min_requests_per_work}, "
-            f"pending_requests_per_running_work={pending_requests_per_running_work}, "
+
+        # # scale out if the number of pending requests exceeds max batch size.
+        # max_requests_per_work = self.max_batch_size
+        # if pending_requests_per_running_or_pending_work >= max_requests_per_work:
+        #     return replicas + 1
+
+        # # scale in if the number of pending requests is below 25% of max_requests_per_work
+        # min_requests_per_work = max_requests_per_work * 0.25
+        # if pending_requests_per_running_or_pending_work < min_requests_per_work:
+        #     return replicas - 1
+
+        return 30
+
+    def autoscale(self) -> None:
+        """Adjust the number of works based on the target number returned by ``self.scale``."""
+        if time.time() - self._last_autoscale < self.autoscale_interval:
+            return
+
+        self.load_balancer.update_servers(self.workers)
+
+        metrics = {
+            "pending_requests": self.num_pending_requests,
+            "pending_works": self.num_pending_works,
+        }
+
+        # ensure min_replicas <= num_replicas <= max_replicas
+        num_target_workers = max(
+            self.min_replicas,
+            min(self.max_replicas, self.scale(self.num_replicas, metrics)),
         )
+        time_requested_at = time.time()
 
-        if pending_requests_per_running_or_pending_work >= max_requests_per_work:
-            print(f"New work requested at: {datetime.datetime.now()} ({replicas} -> {replicas+1})")
-            return replicas + 1
+        # upscale
+        num_workers_to_add = num_target_workers - self.num_replicas
+        for _ in range(num_workers_to_add):
+            print(f"Upscaling from {self.num_replicas} to {self.num_replicas + 1}")
+            work = self.create_work(time_requested_at)
+            new_work_id = self.add_work(work)
+            print(f"Work created: '{new_work_id}'")
 
-        # scale in if the number of pending requests is below 25% of max_requests_per_work
-        if pending_requests_per_running_work < min_requests_per_work:
-            return replicas - 1
+        # downscale
+        num_workers_to_remove = self.num_replicas - num_target_workers
+        for _ in range(num_workers_to_remove):
+            print(f"Downscaling from {self.num_replicas} to {self.num_replicas - 1}")
+            removed_work_id = self.remove_work(self.num_replicas - 1)
+            print(f"Work removed: '{removed_work_id}'")
 
-        return replicas
+        self.load_balancer.update_servers(self.workers)
+        self._last_autoscale = time.time()
 
 
 app = L.LightningApp(
     MyAutoScaler(
         MyPythonServer,
         min_replicas=1,
-        max_replicas=11,
+        max_replicas=30,
         autoscale_interval=1,
         endpoint="predict",
         input_type=RequestModel,
