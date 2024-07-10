91d657e84ef65fedb8eb4e9881dff44f5d50ea55
Akihiro Nitta <nitta@akihironitta.com>
2024-07-10 18:15:20 +0000

    repro

--- pl_boring_model/main.py
+++ pl_boring_model/main.py
@@ -25,38 +25,31 @@ class BoringModel(LightningModule):
         return self.layer(x)
 
     def training_step(self, batch, batch_idx):
-        loss = self(batch).sum()
-        self.log("train_loss", loss)
-        return {"loss": loss}
+        return self(batch).sum()
 
     def validation_step(self, batch, batch_idx):
         loss = self(batch).sum()
-        self.log("valid_loss", loss)
 
     def test_step(self, batch, batch_idx):
         loss = self(batch).sum()
-        self.log("test_loss", loss)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.layer.parameters(), lr=0.1)
+        optimizer = torch.optim.Adam(self.layer.parameters(), lr=torch.tensor(0.1))
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
+        optimizer.step = torch.compile(optimizer.step)
         return [optimizer], [lr_scheduler]
 
 
 def main():
-    train_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    val_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-    test_data = DataLoader(RandomDataset(32, 64), batch_size=2)
-
+    train_data = DataLoader(RandomDataset(32, 16), batch_size=2)
     model = BoringModel()
     trainer = Trainer(
-        max_epochs=1,
-        accelerator="auto",
-        devices="auto",
-        benchmark=False,  # True by default in 1.6.{0-3}.
+        max_epochs=5,
+        enable_checkpointing=False,
+        enable_model_summary=False,
+        enable_progress_bar=False,
     )
-    trainer.fit(model, train_dataloaders=train_data, val_dataloaders=val_data)
-    trainer.test(model, dataloaders=test_data)
+    trainer.fit(model, train_dataloaders=train_data)
 
 
 if __name__ == "__main__":
